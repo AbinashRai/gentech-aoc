7752cc62d51ef2a913c5f56d9250e873bb4ebdca
Abinash Rai <abinashbantawa1905@gmail.com>
2023-06-29 16:47:35 +0545

    worked on responsiveness again

--- src/app/page.js
+++ src/app/page.js
@@ -9,20 +9,22 @@ export default function Home() {
     <div className="w-[375px] sm:w-[640px] md:w-[1080px] mx-auto bg-slate-100 h-screen">
       <div className="container h-[553px] flex justify-between ">
         <div className="w-auto">
-          <div className="w-[214px] h-[28px] mt-[81px]">
+          <div className="w-[160px] md:w-[214px] h-[28px] mt-[81px]">
             <h3 className="flex justify-normal items-center">
               <span className="h-[18px] w-[18px]">
                 <FiCheckCircle />
               </span>
-              <span className="ml-2 text-[16px]">Most trusted platform</span>
+              <span className="ml-2 md:text-[16px] text-[13px]">
+                Most trusted platform
+              </span>
             </h3>
           </div>
           <div className="mt-[16px]">
-            <h1 className="text-[25px] sm:text-[30px] md:text-[45px] font-semibold">
+            <h1 className="text-[22px] sm:text-[25px] md:text-[45px] font-semibold">
               Welcome To <br /> Online Account Portal
             </h1>
             <div className="w-[366px] h-[137px] pt-[38px]">
-              <ul className="text-[15px] md:text-[16px] ">
+              <ul className="text-[12px] md:text-[16px] ">
                 <li className="mb-[11px] flex justify-normal items-center">
                   <GrRadialSelected />
                   <span className="pl-2">Scalable and Robust</span>
@@ -49,22 +51,22 @@ export default function Home() {
           <div className="h-[470px] mt-[62px] bg-white rounded-2xl sm:w-[274px] md:w-[436px]">
             <div className="h-[392px] pt-[7px] border-b-2 w-[274px] md:w-[371px] mx-auto">
               <img
-                className="w-[284px] h-[190px] ml-[47px]"
+                className="w-[180px] md:w-[284px] h-[120px] md:h-[190px] ml-[47px]"
                 src="/images/Apply-for-a-loan-8 1.png"
                 alt="home-image"
               />
-              <div className="mx-auto w-[297px] text-zinc-500 pt-[19px] text-[13px] md:text-[15px]">
+              <div className="mx-auto w-[200px] md:w-[297px] text-zinc-500 pt-[19px] text-[13px] md:text-[15px]">
                 <p>
                   Open your Saving or Fixed Desposit account in just a Few
                   Steps...
                 </p>
                 <div className="mx-auto">
                   <Link
-                    className="flex mx-auto text-[14px]  bg-[#F03738] text-white w-[194px] h-[40px] rounded-lg mb-[55px] mt-[36px]"
+                    className="flex mx-auto text-[10px] md:text-[14px]  bg-[#F03738] text-white w-[130px] md:w-[194px] h-[40px] rounded-lg mb-[55px] mt-[36px]"
                     href="/">
                     <span className="flex justify-normal items-center mx-auto">
                       Open a New Account
-                      <span className="ml-2 text-lg">
+                      <span className="ml-0.5 md:ml-2 text-sm md:text-lg">
                         <FiArrowRight />
                       </span>
                     </span>
