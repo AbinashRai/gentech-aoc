2772268d7bc3d1f137dc04485f84439af02043b1
Abinash Rai <abinashbantawa1905@gmail.com>
2023-07-02 18:02:53 +0545

    adjusted the responsiveness of the home page

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,8 +3,8 @@ import "../globals.scss";
 
 const Footer = () => {
   return (
-    <div className="w-[375px] sm:w-[640px] md:w-[1080px] mx-auto border-2 border-t-0">
-    <div className='container h-[58px] bg-white flex justify-between items-center text-[12px] font-semibold'>
+    <div className=" lg:w-[1080px] mx-auto border-2 border-t-0">
+    <div className='max-[1024px]:w-[83%] lg:w-[928px] mx-auto h-[58px] bg-white flex justify-between items-center text-[12px] font-semibold'>
       Copyright © 2023. All rights reserved. Abc bank
       <div className="cursor-pointer mr-1 flex justify-normal items-center">
         <span className="mr-2 text-[12px]">Powered by:</span>
--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ import { FiArrowRight } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <div className="w-[375px] sm:w-[640px] md:w-[1080px] mx-auto bg-slate-100 h-screen">
-      <div className="container h-[553px] flex justify-between ">
+    <div className=" lg:w-[1080px] mx-auto bg-slate-100 lg:h-screen md:h-fit">
+      <div className="max-[1024px]:w-[83%] lg:w-[928px] mx-auto h-[553px] flex justify-between max-[640px]:block">
         <div className="w-auto">
-          <div className="w-[160px] md:w-[214px] h-[28px] mt-[81px]">
-            <h3 className="flex justify-normal items-center">
+          <div className="w-[160px] md:w-[214px] h-[28px] lg:mt-[81px] md:mt-[55px]">
+            <h3 className="flex justify-normal items-center max-[640px]:pt-6">
               <span className="h-[18px] w-[18px]">
                 <FiCheckCircle />
               </span>
@@ -19,27 +19,27 @@ export default function Home() {
               </span>
             </h3>
           </div>
-          <div className="mt-[16px]">
-            <h1 className="text-[22px] sm:text-[25px] md:text-[45px] font-semibold">
+          <div className="mt-[16px] max-[640px]:mt-[20px]">
+            <h1 className="text-[21px] sm:text-[25px] md:text-[30px] lg:text-[45px] font-semibold">
               Welcome To <br /> Online Account Portal
             </h1>
-            <div className="w-[366px] h-[137px] pt-[38px]">
-              <ul className="text-[12px] md:text-[16px] ">
-                <li className="mb-[11px] flex justify-normal items-center">
+            <div className="max-[640px]:w-[250px] md:w-[366px] h-[137px] max-[640px]:pt-[10px] lg:pt-[38px] md:pt-[18px]">
+              <ul className="text-[12px] lg:text-[16px] md:text-[14px]">
+                <li className="sm:mb-[11px] max-[640px]:mb-[6px] flex justify-normal items-center">
                   <GrRadialSelected />
                   <span className="pl-2">Scalable and Robust</span>
                 </li>
-                <li className="mb-[11px] flex justify-normal items-center">
+                <li className="sm:mb-[11px] max-[640px]:mb-[6px]  flex justify-normal items-center">
                   <GrRadialSelected />
                   <span className="pl-2">
                     Third party Integration Like Nagarik App
                   </span>
                 </li>
-                <li className="mb-[11px] flex justify-normal items-center">
+                <li className="sm:mb-[11px] max-[640px]:mb-[6px]  flex justify-normal items-center">
                   <GrRadialSelected />
                   <span className="pl-2">OCR Scan</span>
                 </li>
-                <li className="mb-[11px] flex justify-normal items-center">
+                <li className="sm:mb-[11px] max-[640px]:mb-[6px] flex justify-normal items-center">
                   <GrRadialSelected />
                   <span className="pl-2">Debit Card</span>
                 </li>
@@ -47,22 +47,22 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="w-[360px]  md:w-[436px] ">
-          <div className="h-[470px] mt-[62px] bg-white rounded-2xl sm:w-[274px] md:w-[436px]">
-            <div className="h-[392px] pt-[7px] border-b-2 w-[274px] md:w-[371px] mx-auto">
+        <div className="w-[360px] lg:w-[436px]  md:w-[272px] max-[640px]:w-full">
+          <div className="lg:h-[470px] md:h-[366px] md:mt-[43px] lg:mt-[62px] bg-white rounded-2xl sm:w-[274px] md:w-[272px] lg:w-[436px]">
+            <div className="lg:h-[392px] md:h-[324px] pt-[7px] border-b-2 w-[274px] lg:w-[371px] md:w-[238px]  mx-auto">
               <img
-                className="w-[180px] md:w-[284px] h-[120px] md:h-[190px] ml-[47px]"
+                className="w-[180px] lg:w-[284px] md:w-[226px]  h-[120px] md:h-[150px] lg:h-[190px] max-[1024px]:mx-auto max-[1024px]:w-[80%] lg:ml-[47px]"
                 src="/images/Apply-for-a-loan-8 1.png"
                 alt="home-image"
               />
-              <div className="mx-auto w-[200px] md:w-[297px] text-zinc-500 pt-[19px] text-[13px] md:text-[15px]">
+              <div className="mx-auto w-[200px] lg:w-[297px] md:w-[238px]  text-zinc-500 pt-[19px] text-[13px] md:text-[14px] lg:text-[15px]">
                 <p>
                   Open your Saving or Fixed Desposit account in just a Few
                   Steps...
                 </p>
                 <div className="mx-auto">
                   <Link
-                    className="flex mx-auto text-[10px] md:text-[14px]  bg-[#F03738] text-white w-[130px] md:w-[194px] h-[40px] rounded-lg mb-[55px] mt-[36px]"
+                    className="flex mx-auto text-[10px] md:text-[14px]  bg-[#F03738] text-white w-[130px] md:w-[194px] h-[40px] rounded-lg md:mb-[35px] md:mt-[25px] lg:mb-[55px] lg:mt-[36px]"
                     href="/">
                     <span className="flex justify-normal items-center mx-auto">
                       Open a New Account
