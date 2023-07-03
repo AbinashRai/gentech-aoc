2f3c6555ad9dd0661435e766e96a62bbc24f81ac
Abinash Rai <abinashbantawa1905@gmail.com>
2023-07-03 11:07:26 +0545

    adjusted the vertical length in responsive screen

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,12 +4,12 @@ import "../globals.scss";
 const Footer = () => {
   return (
     <div className=" lg:w-[1080px] mx-auto border-2 border-t-0">
-    <div className='max-[1024px]:w-[83%] lg:w-[928px] mx-auto h-[58px] bg-white flex justify-between items-center text-[12px] font-semibold'>
-      Copyright © 2023. All rights reserved. Abc bank
-      <div className="cursor-pointer mr-1 flex justify-normal items-center">
-        <span className="mr-2 text-[12px]">Powered by:</span>
+    <div className='max-[1024px]:w-[83%] max-[640px]:text-[11px] lg:w-[928px] mx-auto h-[58px] bg-white flex md:justify-between items-center md:text-[12px] font-semibold max-[640px]:flex-col max-[640px]:justify-center'>
+      <div className="mr-4">Copyright © 2023. All rights reserved. Abc bank</div>
+      <div className="cursor-pointer mr-1 flex justify-normal items-center max-[640px]:justify-center">
+        <span className="mr-2 max-[640px]:text-[11px] md:text-[12px] ">Powered by:</span>
             <Link href="/">
-            <img className="w-[80px] h-[34px]" src="/images/Frame 7.png" alt="logo" />
+            <img className="w-[80px] max-[640px]:h-[30px] md:h-[34px]" src="/images/Frame 7.png" alt="logo" />
             </Link>
         </div>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import { FiArrowRight } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <div className=" lg:w-[1080px] mx-auto bg-slate-100 lg:h-screen md:h-fit">
+    <div className=" lg:w-[1080px] mx-auto bg-slate-100">
       <div className="max-[1024px]:w-[83%] lg:w-[928px] mx-auto h-[553px] flex justify-between max-[640px]:block">
         <div className="w-auto">
           <div className="w-[160px] md:w-[214px] h-[28px] lg:mt-[81px] md:mt-[55px]">
@@ -48,21 +48,21 @@ export default function Home() {
           </div>
         </div>
         <div className="w-[360px] lg:w-[436px]  md:w-[272px] max-[640px]:w-full">
-          <div className="lg:h-[470px] md:h-[366px] md:mt-[43px] lg:mt-[62px] bg-white rounded-2xl sm:w-[274px] md:w-[272px] lg:w-[436px]">
-            <div className="lg:h-[392px] md:h-[324px] pt-[7px] border-b-2 w-[274px] lg:w-[371px] md:w-[238px]  mx-auto">
+          <div className="lg:h-[470px] md:h-[366px] max-[640px]:h-[275px] md:mt-[43px] lg:mt-[62px] bg-white rounded-2xl sm:w-[274px] md:w-[272px] lg:w-[436px]">
+            <div className="lg:h-[392px] md:h-[324px] max-[640px]:h-[250px] pt-[7px] border-b-2 w-[245px] lg:w-[371px] md:w-[238px]  mx-auto">
               <img
                 className="w-[180px] lg:w-[284px] md:w-[226px]  h-[120px] md:h-[150px] lg:h-[190px] max-[1024px]:mx-auto max-[1024px]:w-[80%] lg:ml-[47px]"
                 src="/images/Apply-for-a-loan-8 1.png"
                 alt="home-image"
               />
-              <div className="mx-auto w-[200px] lg:w-[297px] md:w-[238px]  text-zinc-500 pt-[19px] text-[13px] md:text-[14px] lg:text-[15px]">
+              <div className="mx-auto w-[235px] lg:w-[297px] md:w-[238px]  text-zinc-500 max-[640px]:pt-[5px] md:pt-[19px] text-[13px] md:text-[14px] lg:text-[15px]">
                 <p>
                   Open your Saving or Fixed Desposit account in just a Few
                   Steps...
                 </p>
                 <div className="mx-auto">
                   <Link
-                    className="flex mx-auto text-[10px] md:text-[14px]  bg-[#F03738] text-white w-[130px] md:w-[194px] h-[40px] rounded-lg md:mb-[35px] md:mt-[25px] lg:mb-[55px] lg:mt-[36px]"
+                    className="flex mx-auto text-[10px] md:text-[14px] max-[640px]:mt-[15px] bg-[#F03738] text-white w-[130px] md:w-[194px] h-[40px] rounded-lg md:mb-[35px] md:mt-[25px] lg:mb-[55px] lg:mt-[36px]"
                     href="/">
                     <span className="flex justify-normal items-center mx-auto">
                       Open a New Account
